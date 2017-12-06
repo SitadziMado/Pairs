092516e5bf1c5ede943d8455e5214c2cab394e1e
Maxim Suntsev <masuntsev@edu.hse.ru>
2017-12-07 03:00:13 +0500

    Немного доработана инициализация.

--- Pairs/BipartiteGraph.cs
+++ Pairs/BipartiteGraph.cs
@@ -39,6 +39,26 @@ namespace Pairs
             mCount = length;
         }
 
+        private EdgeState this[int firstIndex, int secondIndex]
+        {
+            get
+            {
+                if (0 < firstIndex || firstIndex >= mCount ||
+                    0 < secondIndex || secondIndex >= mCount)
+                    throw new ArgumentOutOfRangeException("Недействительное ребро.");
+
+                return mMatrix[firstIndex][secondIndex];
+            }
+            set
+            {
+                if (0 < firstIndex || firstIndex >= mCount ||
+                    0 < secondIndex || secondIndex >= mCount)
+                    throw new ArgumentOutOfRangeException("Недействительное ребро.");
+
+                    mMatrix[firstIndex][secondIndex] = value;
+            }
+        }
+
         public void Connect(int firstIndex, int secondIndex)
         {
             mMatrix[firstIndex][secondIndex] = EdgeState.Black;
@@ -46,13 +66,20 @@ namespace Pairs
 
         public void FindPairs()
         {
-            for (int i = 0; i < mCount; ++i)
-            {
-                if (!mMatrix[i].Contains(EdgeState.Red))
-                {
+            ClearPrevious();
+            InitialPaint();
 
-                }
-            }
+            bool p(Tuple<int, int> ij) =>
+                !ColHasRed(ij.Item1) && 
+                !RowHasRed(ij.Item2) && 
+                this[ij.Item1, ij.Item2] == EdgeState.Black;
+
+            var fst = FindFirstEdge(p);
+
+            if (fst.Item1 == -1)
+                return;
+
+            // Неопределенная вершина найдена!
         }
 
         private void ClearPrevious()
@@ -62,6 +89,58 @@ namespace Pairs
                 mPreviousFirst[i] = -1;
                 mPreviousSecond[i] = -1;
             }
+
+            for (int i = 0; i < mCount; ++i)
+                for (int j = 0; j < mCount; ++j)
+                    if (this[i, j] == EdgeState.Red)
+                        this[i, j] = EdgeState.Black;
+        }
+
+        private void InitialPaint()
+        {
+            bool p(EdgeState es) => es == EdgeState.Black;
+
+            for (int i = 0; i < mCount; ++i)
+                if (!RowHasRed(i))
+                {
+                    var index = mMatrix[i].FindIndex(p);
+
+                    while (index != -1 || ColHasRed(index))
+                        index = mMatrix[i].FindIndex(index + 1, p);
+
+                    if (index == -1)
+                        throw new UnsupportedGraphException("Граф несвязный.");
+
+                    this[i, index] = EdgeState.Red;
+                    // this[index, i] = EdgeState.Red;
+                }
+        }
+
+        private bool RowHasRed(int index) => mMatrix[index].Contains(EdgeState.Red);
+
+        private bool ColHasRed(int index)
+        {
+            for (int i = 0; i < mCount; ++i)
+            {
+                if (this[index, i] == EdgeState.Red)
+                    return true;
+            }
+            
+            return false;
+        }
+
+        private Tuple<int, int> FindFirstEdge(Predicate<Tuple<int, int>> p)
+        {
+            for (int i = 0; i < mCount; ++i)
+                for (int j = 0; j < mCount; ++j)
+                {
+                    var t = new Tuple<int, int>(i, j);
+
+                    if (p(t))
+                        return t;
+                }
+
+            return new Tuple<int, int>(-1, -1);
         }
 
         private List<List<EdgeState>> mMatrix;
---
+++ Pairs/UnsupportedGraphException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Pairs
+{
+    [Serializable]
+    internal class UnsupportedGraphException : Exception
+    {
+        public UnsupportedGraphException()
+        {
+        }
+
+        public UnsupportedGraphException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedGraphException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnsupportedGraphException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
